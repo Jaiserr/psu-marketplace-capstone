6f10619ac0e1691daa1d95c0a7d2b84de9f2763a
Jericho <jerichobantiquete@gmail.com>
2023-11-15 15:37:05 +0800

    feat: create product form

---
+++ resources/views/seller/products/create.blade.php
@@ -0,0 +1,127 @@
+<x-app-layout>
+    <div class="mx-auto px-4 sm:px-6">
+        <section class="bg-gray-100 py-10">
+            <div class="px-4 mx-auto ">
+                <div class="p-8 px-4 bg-indigo-700 rounded-lg">
+                    <div class="grid grid-cols-1 lg:grid-cols-1 gap-6">
+                        <div>
+                            <form action="#" method="POST" enctype="multipart/form-data">
+                                @csrf
+                                <div class="px-4 mb-6">
+                                    <label for="product_name" class="block mb-2 text-sm font-medium text-white"> Product
+                                        Name</label>
+                                    <input id="product_name"
+                                        class="block w-full px-4 py-3 mb-2 text-sm placeholder-gray-500 bg-white border rounded "
+                                        type="text" name="product_name" placeholder="Enter product name">
+                                </div>
+                                <div class="flex flex-wrap items-center">
+                                    <div class="w-full px-4 mb-6 lg:w-2/4">
+                                        <label for="category" class="block mb-2 text-sm font-medium text-white">
+                                            Category
+                                        </label>
+                                        <div class="relative">
+                                            <select
+                                                class="block w-full px-4 py-3 mb-2 text-sm placeholder-gray-500 bg-white border rounded appearance-none"
+                                                name="category" id="category">
+                                                <option value="" disabled selected>Select</option>
+                                                <option value="Men's Fashion">Men's Fashion</option>
+                                                <option value="Women's Fashion">Women's Fashion</option>
+                                                <option value="Mobile Phones & Gadgets">Mobile Phones & Gadgets</option>
+                                                <option value="Computer & Techs">Computer & Techs</option>
+                                                <option value="School Uniforms and Supplies">School Uniforms and
+                                                    Supplies</option>
+                                                <option value="Food and Drinks">Food and Drinks</option>
+                                                <option value="Musical Instruments">Musical Instruments</option>
+                                            </select>
+                                            <div
+                                                class="absolute inset-y-0 right-0 flex items-center px-2 text-gray-500 pointer-events-none">
+                                                <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 20 20">
+                                                    <path
+                                                        d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z">
+                                                    </path>
+                                                </svg>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="w-full px-4 mb-6 lg:w-2/4">
+                                        <label for="condition" class="block mb-2 text-sm font-medium text-white">
+                                            Condition
+                                        </label>
+                                        <div class="relative">
+                                            <select id="condition"
+                                                class="block w-full px-4 py-3 mb-2 text-sm placeholder-gray-500 bg-white border rounded appearance-none"
+                                                name="condition">
+                                                <option value="" disabled selected>Select</option>
+                                                <option value="New">New</option>
+                                                <option value="Used (Like new)">Used (Like new)</option>
+                                                <option value="Used (Good)">Used (Good)</option>
+                                                <option value="Used (Fair)">Used (Fair)</option>
+                                            </select>
+                                            <div
+                                                class="absolute inset-y-0 right-0 flex items-center px-2 text-gray-500 pointer-events-none">
+                                                <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 20 20">
+                                                    <path
+                                                        d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z">
+                                                    </path>
+                                                </svg>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="flex flex-wrap items-center">
+                                    <div class="w-full px-4 mb-6 lg:w-2/4">
+                                        <label for="price" class="block mb-2 text-sm font-medium text-white"> Product
+                                            Price</label>
+                                        <input
+                                            class="block w-full px-4 py-3 mb-2 text-sm placeholder-gray-500 bg-white border rounded "
+                                            type="number" name="price" id="price" placeholder="Enter product price">
+                                    </div>
+                                    <div class="w-full px-4 mb-6 lg:w-2/4">
+                                        <label for="availability" class="block mb-2 text-sm font-medium text-white">
+                                            Availability
+                                        </label>
+                                        <div class="relative">
+                                            <select id="availability"
+                                                class="block w-full px-4 py-3 mb-2 text-sm placeholder-gray-500 bg-white border rounded appearance-none"
+                                                name="availability">
+                                                <option value="" disabled selected>Select</option>
+                                                <option value="Available">Available</option>
+                                                <option value="Sold">Sold</option>
+                                            </select>
+                                            <div
+                                                class="absolute inset-y-0 right-0 flex items-center px-2 text-gray-500 pointer-events-none">
+                                                <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 20 20">
+                                                    <path
+                                                        d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z">
+                                                    </path>
+                                                </svg>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="px-4 mb-6">
+                                    <label for="images" class="block mb-2 text-sm font-medium text-white"
+                                        for="file_input">Upload
+                                        file</label>
+                                    <input name="images[]" multiple accept="image/*" id="images"
+                                        class="block w-full text-sm text-gray-600 border border-gray-200 rounded cursor-pointer file:hover:bg-gray-200 file:border-solid file:border-0 file:cursor-pointer  file:mr-5 file:px-5 file:py-3 bg-gray-50   file:border-r file:border-gray-300 file:bg-gray-100 "
+                                        type="file">
+                                </div>
+                                <div class="px-4 ">
+                                    <div class="flex ">
+                                        <button type="button"
+                                            class="inline-block px-6 py-2.5 bg-blue-500  text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-600 hover:shadow-lg ">Submit</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</x-app-layout>
