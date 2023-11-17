3673f00c4de31ff4ce0473fe35305f788c9ac348
Jericho <jerichobantiquete@gmail.com>
2023-11-17 15:46:14 +0800

    sold

--- resources/views/customer/dashboard/index.blade.php
+++ resources/views/customer/dashboard/index.blade.php
@@ -3,21 +3,6 @@
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="flex gap-2 items-center justify-between">
                 <h2 class="text-lg">Available Products</h2>
-                <div class="relative">
-                    <div class="ml-4 flow-root lg:ml-6">
-                        <a href="#" class="group -m-2 flex items-center p-2">
-                            <svg class="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" fill="none"
-                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-                            </svg>
-                            <a class="block" href="{{ route('wishlist.index') }}">Wishlist</a>
-                            <span class="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-                                {{ $wishlistItemsCount }}
-                            </span>
-                        </a>
-                    </div>
-                </div>
             </div>
             <section class="flex items-center mt-10">
                 <div class="p-4 mx-auto ">
@@ -47,6 +32,7 @@
                                     Seller: {{ $product->user->name }}
                                 </p>
 
+                                @if ($product->availability === "Available")
                                 <form action="{{ route('wishlist.add', $product) }}" method="POST">
                                     @csrf
                                     <button
@@ -59,6 +45,13 @@
                                     class="transform active:scale-[.98] mt-2 flex w-full items-center justify-center rounded-md border border-transparent bg-[#DE3151] py-3 px-8 text-base font-medium text-white hover:bg-[#d22544] focus:outline-none focus:ring-2 focus:ring-[#DE3151] focus:ring-offset-2">
                                     Message
                                 </a>
+
+                                @else
+                                <button
+                                    class="transform active:scale-[.98] mt-2 flex w-full items-center justify-center rounded-md border border-transparent bg-[#DE3151] py-3 px-8 text-base font-medium text-white hover:bg-[#d22544] focus:outline-none focus:ring-2 focus:ring-[#DE3151] focus:ring-offset-2">
+                                    Sold
+                                </button>
+                                @endif
                             </div>
                         </div>
                         @endforeach
